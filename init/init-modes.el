79855e8a1a5fa161e2a0046797877e319cd0cc77
Charlie Brown <stieizc.33@gmail.com>
2015-07-08 15:16:26 +0800

    change coffee indent

--- init/init-modes.el
+++ init/init-modes.el
@@ -5,24 +5,35 @@
 
 ;;; Code:
 
+;; Python
 (add-hook
  'python-mode-hook
  (lambda ()
    (add-to-list
     (make-local-variable 'company-backends)
     'company-jedi)))
+
+;; CoffeeScript Mode
+(custom-set-variables '(coffee-tab-width 2))
+
+;; Completion
 (global-company-mode)
+
+;; Flycheck
 (global-flycheck-mode)
 
+;; Helm
 (helm-mode 1)
 (helm-autoresize-mode 1)
+
+;; Projectile
 (projectile-global-mode)
 (setq projectile-completion-system 'helm)
 (helm-projectile-on)
 
+;; Parens
 (show-paren-mode 1)
 (setq show-paren-delay 0)
-
 (electric-pair-mode)
 
 (provide 'init-modes)
