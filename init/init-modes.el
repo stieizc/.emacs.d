4a325ea9f675e85fa31364fac58a7fd19292cf80
Charlie Brown <stieizc.33@gmail.com>
2015-07-14 19:24:05 +0800

    Markdown customize motion keys

--- init/init-modes.el
+++ init/init-modes.el
@@ -22,6 +22,18 @@
 ;; Apiary
 (add-to-list 'auto-mode-alist '("\\.apib$" . markdown-mode))
 
+;; Markdown
+(add-hook 'markdown-mode-hook 'outline-evil-keys)
+
+;; Outline
+(defun outline-evil-keys()
+  (progn
+    (define-key evil-normal-state-map (kbd "C-n") 'outline-next-visible-heading)
+    (define-key evil-normal-state-map (kbd "C-p") 'outline-previous-visible-heading)
+    (define-key evil-normal-state-map (kbd "C-f") 'outline-forward-same-level)
+    (define-key evil-normal-state-map (kbd "C-b") 'outline-backward-same-level)
+    (define-key evil-normal-state-map (kbd "C-u") 'outline-up-heading)))
+
 ;; Completion
 (global-company-mode)
 
