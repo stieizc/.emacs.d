338b4fe6b7aa7be5a1f2edf466c68badef45e7ec
Charlie Brown <stieizc.33@gmail.com>
2015-10-14 21:02:50 +0800

    add web mode

--- init/init-modes.el
+++ init/init-modes.el
@@ -94,6 +94,17 @@
 ;; Org mode
 (eval-after-load "org" '(setcdr (assoc "\\.pdf\\'" org-file-apps) "pupdf %s"))
 
+;; Web mode
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-hook
+ 'web-mode-hook
+ (lambda ()
+   (setq web-mode-markup-indent-offset 2)
+   (setq web-mode-css-indent-offset 2)
+   (setq web-mode-code-indent-offset 2)
+   ))
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
 (provide 'init-modes)
 
 ;;; init-modes.el ends here
