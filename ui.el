5a53f93dbe24567dafb09eb999f20d69242be747
Wenxin Wang <i@wenxinwang.me>
2019-02-10 16:35:09 +0800

    neotree & theme

--- ui.el
+++ ui.el
@@ -12,4 +12,17 @@
 
 (use-package all-the-icons)
 
+(use-package neotree
+  :config
+  (evil-leader/set-key
+    "pt" 'neotree-toggle))
+
+(use-package doom-themes
+  :config
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+	doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-city-lights t)
+  (doom-themes-neotree-config)
+  (doom-themes-org-config))
+
 ;;; ui.el ends here
