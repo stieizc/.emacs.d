e3564e40a38aa6e76cc8cd897f62e2aba5d67451
Wenxin Wang <i@wenxinwang.me>
2019-02-10 14:46:38 +0800

    simpler hybrid mode
    Seems that simply copy code from spacemacs isn't good enough,
    evil-insert-state-map still shadows some keys.
    
    Spacemacs does a lot to ensure smooth switching between vim-mode and
    hybrid-mode, but currently I don't need that kind of switching. So I
    use a simpler method copied from stackoverflow, that essentially
    clears evil-insert-state-map.

---
+++ init-evil-hybrid-mode.el
@@ -0,0 +1,20 @@
+;;; init-evil-hybrid-mode --- evil hybrid mode from spacemacs
+
+;;; Commentary:
+
+;;; Code:
+
+;; hybrid mode, like spacemacs
+;; https://github.com/syl20bnr/spacemacs/blob/master/layers/%2Bdistributions/spacemacs-base/local/hybrid-mode/hybrid-mode.el
+
+;; When I use code from spacemacs, C-k/C-y still gets shadowed by evil-insert-state-map
+;; So I use code from here
+;; https://stackoverflow.com/questions/25542097/emacs-evil-mode-how-to-change-insert-state-to-emacs-state-automatically/28985130#28985130
+
+(defconst evil-insert-state-map-backup evil-insert-state-map
+  "Backup of `evil-insert-state-map'.")
+
+(setq evil-insert-state-map (make-sparse-keymap))
+(define-key evil-insert-state-map (kbd "<escape>") 'evil-normal-state)
+
+;;; init-evil-hybrid-mode.el ends here
