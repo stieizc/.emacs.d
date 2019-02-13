52cd34e9dae968193b44cfbbbc82d217233b9922
Wenxin Wang <i@wenxinwang.me>
2019-02-13 13:41:50 +0800

    company, flycheck, lsp-mode, ccls

--- lang.el
+++ lang.el
@@ -12,35 +12,85 @@
   :config
   (editorconfig-mode 1))
 
-;;; Clang-format
+;;; - General packages
+
+;; Clang-format
+
+(use-package clang-format
+  :commands clang-format-region)
+
+;; Flycheck
+(use-package flycheck
+  :config
+  ;; for ccls
+  (setq-default
+   flycheck-disabled-checkers
+   '(c/c++-clang c/c++-cppcheck c/c++-gcc)))
+  ;; (global-flycheck-mode)) It's annoyting sometimes
+
+;;; Company
+(use-package company
+  :config
+  (global-company-mode 1))
+
+;;; lsp-mode
+(use-package lsp-mode
+  :commands lsp
+  :init
+  (setq lsp-auto-configure nil
+	lsp-prefer-flymake nil
+	lsp-session-file (expand-file-name ".lsp-session-v1" my:cache-dir)))
+
+(use-package lsp-ui
+  ;; :hook lsp-mode seems to add a hook called lsp-ui, not lsp-ui-mode
+  :commands lsp-ui-mode
+  :init
+  (add-hook 'lsp-mode-hook #'lsp-ui-mode))
+
+;;; company
+(use-package company-lsp
+  :requires company
+  :config
+  (push 'company-lsp company-backends))
+
+;;; - C
+
+;; ccls
+(use-package ccls
+  :defer t)
 
 (defvar-local my:c-common-use-clang-format t
   "use clang-format for regiion indent, default to t")
 
 (defun my:c-common-hook ()
+  (require 'ccls)
+  (lsp)
+  (flycheck-mode)
   (when my:c-common-use-clang-format
     (make-local-variable 'indent-region-function)
     (setq indent-region-function #'clang-format-region)))
 
-(use-package clang-format
-  :config
-  (add-hook 'c-mode-common-hook #'my:c-common-hook))
+(add-hook 'c-mode-common-hook #'my:c-common-hook)
 
-;;; Elisp setup
+;;; - Elisp setup
 
 ;; https://emacsredux.com/blog/2013/06/25/boost-performance-by-leveraging-byte-compilation/
 (defun er:remove-elc-on-save ()
   "If you're saving an Emacs Lisp file, likely the .elc is no longer valid."
   (add-hook
    'after-save-hook
-   (lambda ()
+   #'(lambda ()
      (if (file-exists-p (concat buffer-file-name "c"))
 	 (delete-file (concat buffer-file-name "c"))))
    nil
    t))
-(add-hook 'emacs-lisp-mode-hook 'er:remove-elc-on-save)
 
-;;; Haskell setup
+(defun my:elisp-mode-hook ()
+  (er:remove-elc-on-save))
+
+(add-hook 'emacs-lisp-mode-hook #'my:elisp-mode-hook)
+
+;;; - Haskell setup
 
 (use-package haskell-mode
   :defer
