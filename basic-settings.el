1ff76f3494db8d7b670ee8c575ec68070491f92e
Wenxin Wang <i@wenxinwang.me>
2019-03-02 19:50:30 +0800

    if set utf8 too much, detection for gb18030 wont work
    https://stackoverflow.com/questions/20474591/which-coding-system-should-i-use-in-emacs-when-i-copy-chinese-words-to-emacs-t

--- basic-settings.el
+++ basic-settings.el
@@ -80,13 +80,14 @@
 ;; UTF-8 as the default coding system
 (when (fboundp 'set-charset-priority)
   (set-charset-priority 'unicode))
+(prefer-coding-system        'chinese-gb18030)
 (prefer-coding-system        'utf-8)
-(set-terminal-coding-system  'utf-8)
-(set-keyboard-coding-system  'utf-8)
-(set-selection-coding-system 'utf-8)
-(setq locale-coding-system   'utf-8)
-(setq-default buffer-file-coding-system 'utf-8)
-(set-language-environment "UTF-8")
+;; (set-terminal-coding-system  'utf-8)
+;; (set-keyboard-coding-system  'utf-8)
+;; (set-selection-coding-system 'utf-8)
+;; (setq locale-coding-system   'utf-8)
+;; (setq-default buffer-file-coding-system 'utf-8)
+;; (set-language-environment "UTF-8")
 
 ;;; - Version control
 (setq
