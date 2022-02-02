33279ea0c010af68b271e2d339f64dcbb0f8790c
Wenxin Wang <i@wenxinwang.me>
2022-02-02 23:07:11 +0800

    comment on orgmode config

--- lisp/init-orgmode.el
+++ lisp/init-orgmode.el
@@ -4,6 +4,9 @@
   :straight nil
   :init
   (setq
+   ;; Without this org mode will add indentation
+   ;; after headings.
+   ;; Also, use M-return for inserting headlines!
    org-adapt-indentation nil))
 
 (provide 'init-orgmode)
