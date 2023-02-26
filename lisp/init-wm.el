e1d3feb0e25a8a3625fa90a2da15da9822b3e7ea
Wenxin Wang <i@wenxinwang.me>
2023-02-26 21:41:41 +0800

    wm: theme & mode line

--- lisp/init-wm.el
+++ lisp/init-wm.el
@@ -139,4 +139,33 @@
   :demand t
   :config
   (pyim-wbdict-v86-enable))
+
+(use-package doom-themes
+  :demand t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-opera-light t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+(use-package doom-modeline
+  :hook (after-init . doom-modeline-mode)
+  :config
+  (setq doom-modeline-enable-word-count t)
+  (setq doom-modeline-buffer-encoding nil)
+  (setq doom-modeline-github t))
+
+(use-package all-the-icons
+  :demand t)
+
 (provide 'init-wm)
